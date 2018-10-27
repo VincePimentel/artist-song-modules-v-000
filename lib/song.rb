d536fabf6242dc90c1a8dff464f939d86366cc5c
Vince Andrew Pimentel <contact@vincep.me>
2018-10-27 20:15:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 # require_relative './concerns/memorable.rb'
 
 class Song
-  extend Memorable, Findable, Paramble
+  extend Memorable, Findable, Paramable
 
   attr_accessor :name
   attr_reader :artist
